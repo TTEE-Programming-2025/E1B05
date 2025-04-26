4f06f7107b7f3795831fe074e0e742c46b4e0246
chen-yu-hung <u11302205@o365.ttu.edu.tw>
2025-04-26 21:34:06 +0800

    完成2.3
    直角三角形副程式，(輸入錯誤)修正

--- HW2.c
+++ HW2.c
@@ -2,23 +2,73 @@
 #include<stdlib.h>
 void menu1(int);
 void menu2(int);
+void RT(int);
 
 int main(void)
 {
-	int n=1,i,j,password;//宣告變數 
-	char ch;
+	int n=0,i=1,a=0,password;//宣告變數 
+	char ch,chRT='a';
 	menu1(1);
 	do
 	{
 		printf("請輸入密碼:(密碼預設為2025)\n");
 	    scanf("%d",&password);
 	    if(password==2025)
-	        menu2(1);
+	    {
+		
+	        while(1)
+	        {
+			    menu2(1);
+	            while(1)
+	            {
+	            	printf("請輸入:");
+	            	scanf(" %c",&ch);
+	            	switch(ch)
+	            	{
+	            		case 'a':
+	            		case 'A':
+	            			system("CLS"); // 清除螢幕
+	            			while(1)
+	        		    	{
+	        		    		//system("CLS"); // 清除螢幕
+	        		    		printf("請輸入a-n:\n");
+	        		    		scanf(" %c",&chRT);
+	        		    		//chRT=getch();
+	        		    		if((int)chRT<'a'||(int)chRT>'n')
+	        		    		{
+	        		    	        printf("輸入錯誤，請重新輸入\n");
+	        		    	        //system("pause");
+	        		    	        //continue;
+	        		    	    }
+	        		    	    else
+	        		    	    {
+	        		    	    	RT((int)chRT-96);
+	        		    	    	system("pause");
+	        		    	    	break;
+								}
+	        		    		//scanf("%c",&chRT);
+	        		    		//system("pause");
+	        		    		//a=(int)chRT-96;
+	        		    	        
+	        		    	    //system("pause");
+	        		    	    //break;
+	        		    	    
+	        		    	    //system("pause");
+					    	}//while((int)chRT<'a'||(int)chRT>'n');
+					    	break;
+				    }
+				    if((ch<'a'&&ch>'C')||ch>'c'||ch<'A')
+				        printf("輸入錯誤，請重新輸入\n");
+				    else
+				        break;
+			    }
+			}//while((ch<'a'&&ch>'C')||ch>'c'||ch<'A');
 	        break;
-	    n++;
-	    if(n==4)
+    	}
+	    i++;
+	    if(i==4)
 	        printf("已連續密碼錯誤3次\n");
-	}while(n<=3);
+	}while(i<=3);
     
 	system("pause");
 	return 0;
@@ -42,4 +92,21 @@ void menu2(int a)
 	printf("| b. 顯示乘法表     |\n");
 	printf("| c. 結束           |\n");
 	printf("---------------------\n");
+}
+void RT(int a)
+{
+	int i=0,j=0,k=0;
+	char str[]="abcdefghijklmn";
+	//printf("%d",a);
+	for(i=1;i<=a;i++)
+	{
+		for(j=a;j>i;j--)
+		    printf(" ");
+		for(k=1;k<=i;k++)
+		    printf("%c",str[a-i+k-1]);
+		printf("\n");
+	}
+	//system("pause");
+	return;
+	
 }

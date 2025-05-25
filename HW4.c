de8a8ae740486e1789005a50f0477b3320ffddcc
chen-yu-hung <u11302205@o365.ttu.edu.tw>
2025-05-25 20:57:59 +0800

    完成4.3
    判斷選項a，需要n位學生，輸入姓名、學號、成績，以及輸入錯誤提示

--- HW4.c
+++ HW4.c
@@ -3,9 +3,20 @@
 #include<string.h>
 void personal_screen(void);
 void menu(void);
+void option_a(void);
+
+struct man
+    {
+	    char name[9];
+	    char ID[7];
+	    int math;
+	    int physics;
+	    int english;
+    };
 int main(void)
 {
 	int i,pwd;
+	char option;
 	
 	personal_screen();
 	for(i=0;i<=2;i++)
@@ -18,6 +29,15 @@ int main(void)
 			puts("歡迎!!!");
 			system("pause");
 			menu();
+			printf("選項:");
+			scanf(" %c",&option);
+			fflush(stdin);
+			switch(option)
+			{
+				case 'a':
+					option_a();
+					break;
+			}
 			break;
 		}
 		if(i==2)
@@ -56,4 +76,61 @@ void menu(void)
 	printf("| d. Grade ranking               |\n");
 	printf("| e. Exit system                 |\n");
 	printf("----------------------------------\n");
+}
+void option_a(void)
+{
+	int i,j,n,ok;
+	system("CLS");
+	do
+	{
+		printf("輸入n:(5-10)");
+		scanf(" %d",&n);
+		fflush(stdin);
+		if(n<5||n>10)
+		    printf("輸入錯誤\n");
+	}while(n<5||n>10);
+	struct man student[n];
+	for(i=0;i<n;i++)
+	{
+		printf("輸入第%d位學生姓名:",i+1);
+		scanf(" %s",student[i].name);
+	    do
+	    {
+	    	printf("輸入第%d位學生學號:(6位整數)",i+1);
+	    	scanf(" %s",student[i].ID);
+	    	for(j=0;j<=5;j++)
+	    	{
+	    		ok=0;
+		        if(student[i].ID[j]<'0'||student[i].ID[j]>'9')
+		        {
+		    	    printf("輸入錯誤\n");
+		    	    break;
+			    }
+			    else
+			        ok=1;
+			}
+		}while(strlen(student[i].ID)!=6||ok==0);
+		do
+		{
+			printf("輸入第%d位學生數學成績:",i+1);
+			scanf(" %d",&student[i].math);
+			if(student[i].math<0||student[i].math>100)
+		        printf("輸入錯誤\n");
+		}while(student[i].math<0||student[i].math>100);
+		do
+		{
+			printf("輸入第%d位學生物理成績:",i+1);
+			scanf(" %d",&student[i].physics);
+			if(student[i].physics<0||student[i].physics>100)
+		        printf("輸入錯誤\n");
+		}while(student[i].physics<0||student[i].physics>100);
+		do
+		{
+			printf("輸入第%d位學生英文成績:",i+1);
+			scanf(" %d",&student[i].english);
+			if(student[i].english<0||student[i].english>100)
+		        printf("輸入錯誤\n");
+		}while(student[i].english<0||student[i].english>100);
+		puts("\n");
+	}
 }
